3c29a0f9f7402cb5c57fde4d23596dfff9ffc0b0
Benny Li <59155001+Bennyli1995@users.noreply.github.com>
2023-09-24 23:35:00 -0700

    initial products setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
-
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
+import HomeScreen from "./screens/HomeScreen";
 const App = () => {
-  return <div>Welcome to Proshop</div>;
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to Proshop</h1>
+          <HomeScreen />
+        </Container>
+        <Footer />
+      </main>
+    </>
+  );
 };
 
 export default App;
